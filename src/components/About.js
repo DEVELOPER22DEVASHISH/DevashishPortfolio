ce3d0e36f2fadc5d14734ad2eeea5d2e5501d5ca
DEVASHISH BISWAS <121604662+DEVELOPER22DEVASHISH@users.noreply.github.com>
2023-04-26 19:33:09 +0530

    portfolio link added for accessing through github

--- src/components/About.js
+++ src/components/About.js
@@ -13,6 +13,11 @@ const About = () => {
                     My name is Devashish Biswas. I am from Rudrapur Uttarakhand. I have done my schooling from Uttakhand board & then I pursued B.tech in Electronics & Communication from APJ. Abdul Kalam Technical University Lucknow.
                     Being a B.tech graduate in ECE and working in a corporate office it's been very tough to get into programming & not only I learnt programming, I have learnt it without enrolling in a certification.Talking about my
                     journey till today itself I have completed two full stack projects & it's working perfectly fine, after so many attempts . It feels great. Really really great.
+                    
+                    i am sharing my portfolio link here to check it from github code
+        
+                    https://clever-granita-cfabd0.netlify.app/
+                                    
                 </p>
 
                 <br />
@@ -23,7 +28,7 @@ const About = () => {
                     I used to open multi window to read tutorials in spare time. I just don't want to be fail and not to think by the recruiters that he is from non CS background & he must be a little knowledge , therefore I started to learn
                     all the major language's core concepts like Java ,JavaScript , C, C++, C#, Python, Typescript, PHP, then frameworks like Django, Angular, React Js, BootStrap, TailwindCSS , Node Js, Express Js, Laravel, Spring Boot &
                     also go through the tutorial for GIT & GitHub.Be familiar also with databases like mySql , MongoDb. It's been around 9 months I am learning I also faced so many failure while making project because taking tutorials and
-                    reading theory is different thing and making projects is different thing. I almost 9-10 times failed in making project & it's been so much frustrating after so much effort and time if project gets failed. But I did not
+                    reading theory is different thing and making projects is different thing. I almost 9-10 times failed in making project & it's been so much demotivating after so much effort and time if project gets failed. But I did not
                     give up. Finally I have been succeed and able to successfully completed 2 Full Stack Projects.It's awesome!!! & belive me it took lots of efforts, hard work & patience.. At present I am very happy at this stage of gaining
                     skills and I have firm belive that this portfolio is going to be much much bigger in upcoming days...
                 </p>
